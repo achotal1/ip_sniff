4af48f962e519d8df092f090428ddcdd4857cb6d
achotal1 <achotal1@binghamton.edu>
2023-09-04 13:39:41 -0400

    fixed bugs

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,104 @@
 #![allow(dead_code)]
-use std::env;
+
+use std::env::args;
+use std::io::Write;
 use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
-use std::process;
-use std::sync::mpsc::{Sender, channel};
+use std::{process, io};
+use std::sync::mpsc::{channel, Sender};
 use std::thread;
-use std::io::{self, Write};
 
 const MAX: u16 = 65535;
 
 #[derive(Debug)]
-struct Arguments{	
-	flag: String,
-	ipaddress: IpAddr,
-	threads: u16
+struct Store{
+	flag: String, 
+	threads: u16,
+	ip_addr: IpAddr
 }
 
-impl Arguments{
-	fn new(args: &Vec<String> ) -> Result<Arguments, &'static str>{
+impl Store{
+	fn new(args: &Vec<String>) -> Result<Store, &'static str>{
 		if args.len() < 2{
-			return Err("Not enough arguments");
-		}else if args.len() > 4{
-			return Err("Too many arguments");
+			return Err("Need more arguments");
+		}
+		else if args.len() > 4{
+			return Err("Too many Arguments");
 		}
 		let f = args[1].clone();
-		if let Ok(ipaddr) = IpAddr::from_str(&f){
-			return Ok(Arguments{flag: String::from(""), ipaddress: ipaddr, threads: 4});
+		if let Ok(ip) = IpAddr::from_str(&f){
+			return Ok(Store{ flag: String::from(""), threads: 4, ip_addr: ip});
 		}else{
 			let flag = args[1].clone();
-			if flag == "-h" || flag == "-help"{
-				if args.len() > 2 {
-					return Err("Too many arguments");
-				}else{
-					println!("Usage: -j for adding number of threads
-							  \n -h or -help for this message");
-					return Err("Help");
+			if flag.contains("-h") || flag.contains("-help") {
+				println!("-h or -help to print this message 
+					or -j to add thread number");
+				return Err("help called");
+			}else if flag.contains("-j"){
+				if args.len() < 4{
+					return Err("Need more arguments");
 				}
+				
+				let ip = match IpAddr::from_str(&args[3].clone()){
+								 Ok(s) => s,
+								 Err(_) => return Err("Invalid IP addr")
+								};
+				let thread_number = match args[2].parse::<u16>(){
+									Ok(s) => s,
+									Err(_) => return Err("Thread number cant be parsed")
+								};
+
+				return Ok(Store{flag: flag, threads: thread_number, ip_addr: ip});
+
+			}else{
+				return Err("Invalid syntax");
 			}
-			else if flag == "-j"{
-				let ipaddr = match IpAddr::from_str(&args[3].clone()){
-									Ok(ans) => ans,
-									Err(_) => return Err("IP Address is not valid")
-							};
-				let thread = match args[2].parse::<u16>(){
-									Ok(ans) => ans,
-									Err(_)  => return Err("failed to parse thread number")
-							  };
-				return Ok(Arguments{flag: f,ipaddress: ipaddr,threads: thread});
-			}
-			else{
-				return Err("Invalid Syntax");
-			}
-		
 		}
 	}
 }
-
-fn scan(tx: Sender<u16>, start_port: u16, ipaddr: IpAddr, num_threads: u16){
+fn scan(tx: Sender<u16>, start_port: u16, ipaddr: IpAddr, thread_no: u16 ){
 	let mut port: u16 = start_port + 1;
-	loop {
+	loop{
 		match TcpStream::connect((ipaddr, port)){
 			Ok(_) => {
-				print!(".");
-				io::stdout().flush().unwrap();
-				tx.send(port).unwrap();
-					}
+						print!(".");
+						io::stdout().flush().unwrap();
+						tx.send(port).unwrap();
+					 }
 			Err(_) => {}
-		};
-
-		if (MAX - start_port) <= num_threads{
+		}
+		if MAX - port <= thread_no{
 			break;
 		}
-		port += num_threads;
+		port += thread_no;
 	}
+
 }
 
 
-fn main() {
-	let args: Vec<String> = env::args().collect();
-	let program = args[0].clone();
-	let m: Arguments = Arguments::new(&args).unwrap_or_else(
-						|err| {
-							if err.contains("help"){
-								process::exit(0);
-							}else{
-								eprintln!("{0} problem parsing arguments: {1}", program, err);
+fn main(){
+	let args: Vec<_> = args().collect();
+	let program_name = &args[0];
+	let store: Store = Store::new(&args).unwrap_or_else(
+							|_| {
 								process::exit(0);
 							}
-
-						});
-	let num_threads = m.threads;
-	let addr = m.ipaddress;
+						);
 	let (tx, rx) = channel();
-	for i in 0..num_threads{
-		let tx = tx.clone();
-		thread::spawn(move ||{
-			scan(tx, i, addr, num_threads);
-		});
-
+	let thread_no = store.threads;
+	let ipaddr = store.ip_addr;
+	for i in 0..thread_no{
+		let t = tx.clone();
+		thread::spawn(
+					move ||{
+						scan(t, i, ipaddr, thread_no);
+					});
 	}
-
 	let mut out = vec![];
 	drop(tx);
-
-	for p in rx{
-		out.push(p);
+	for i in rx{
+		out.push(i);
 	}
-	println!("");
 	out.sort();
-	for j in out{
-		println!("{}", j);
-	}
-
-
+	println!("{:?}", out);
 }
